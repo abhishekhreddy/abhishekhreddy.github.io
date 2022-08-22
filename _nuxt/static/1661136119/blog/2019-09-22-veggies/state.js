24fe74e459a93eae092bb10989244963f3955356
Abhishek Hanumantha Reddy <abhi@Abhisheks-MacBook-Pro.local>
2022-08-21 22:44:55 -0700

    Changes4

--- _nuxt/static/1661136119/blog/2019-09-22-veggies/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661136119",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F2019-09-22-veggies",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"60f6cd3c"}},_img:{"/_ipx/_/author.png":"\u002F_nuxt\u002Fimage\u002Ff10964.png"}}}(null));
